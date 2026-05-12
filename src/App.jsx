1c00be240b5cf710dd14ed591428154988c30e33
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-05-12 19:14:03 -0300

    Tenho que arrumar o temporizador

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 
+import { useState } from 'react'
 import './index.css'
 
 function App() {
+
+  const [timer, seTimer] = useState(null)
+  const [restTimer, setRestTimer] = useState(false)
+
+  const timerDecrease = () => {
+    const min = 1
+    const seconds = 59
+    setTimer(`${min}:${seconds} restantes`)
+
+    const startTime = setInterval(() => {
+      while(seconds != 0) {
+        seconds - 1
+      }
+    }, 1000)
+
+    return () => clearInterval(startTime)
+  }
 
 
   return (
     <>
 
       <div className="area_counter">
-        <div id='timer'>⏱️ 2:15 </div>
+        <div id='timer'>⏱️ { restTimer === null ? timer : 'tempo' }</div>
       </div>
       
       <main id='container'>
@@ -22,6 +40,8 @@ function App() {
         <div className='square'>❌</div>
         <div className='square'>⭕</div>
       </main>
+
+      <button onClick={timerDecrease}>Começar</button>
       
            
   
